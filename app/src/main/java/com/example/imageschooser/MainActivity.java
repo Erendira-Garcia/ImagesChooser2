a4afd16bc6ade0975bf69fea86e254b4860cdb65
Erendira-Garcia <iraisegv@gmail.com>
2023-02-27 00:27:05 -0600

    Proyecto

---
+++ app/src/main/java/com/example/imageschooser/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.imageschooser;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView frame;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        frame = findViewById(R.id.imageView);
+
+        //TODO ask for permission of camera upon first launch of application
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) ==
+                    PackageManager.PERMISSION_DENIED ||
+                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                            == PackageManager.PERMISSION_DENIED){
+                String[] permission = {Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                requestPermissions(permission, 112);
+            }
+        }
+
+        //TODO captue image using camera
+        frame.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if (checkSelfPermission(Manifest.permission.CAMERA) ==
+                            PackageManager.PERMISSION_DENIED ||
+                            checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                                    == PackageManager.PERMISSION_DENIED){
+                        String[] permission = {Manifest.permission.CAMERA,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        requestPermissions(permission, 121);
+                    }
+                    else {
+                        openCamera();
+                        //cameraLauncher.launch(new Intent(MediaStore.ACTION_IMAGE_CAPTURE));
+                    }
+                }
+                else {
+                    openCamera();
+                    //cameraLauncher.launch(new Intent(MediaStore.ACTION_IMAGE_CAPTURE));
+                }
+                return false;
+            }
+        });
+    }
+
+    Uri image_uri;
+    private static final int RESULT_LOAD_IMAGE = 123;
+    public static final int IMAGE_CAPTURE_CODE = 654;
+    //TODO opens camera so that user can capture image
+
+ /*   ActivityResultLauncher<Intent> cameraLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if(result.getResultCode() == RESULT_OK){
+                Bundle extras = result.getData().getExtras();
+                Bitmap imgBitmap = (Bitmap) extras.get("data");
+                frame.setImageBitmap(imgBitmap); //imgView podría ser frame
+
+            }
+        }
+    });  */
+
+    //Funcion1
+    private void openCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
+                image_uri =
+                        getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        Intent cameraIntent = new
+                Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+        startActivityForResult  (cameraIntent, IMAGE_CAPTURE_CODE);
+    }
+
+    //Funcion2
+     @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == IMAGE_CAPTURE_CODE && resultCode == RESULT_OK)
+        {
+            frame.setImageURI(image_uri);
+        }
+    }
+}
